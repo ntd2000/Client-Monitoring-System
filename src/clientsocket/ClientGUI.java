a66f9fa4c02a32d1a827b33688d5bd7cf472e625
ntd2000 <thanhphuc2k13@gmail.com>
2022-08-26 12:32:09 +0700

    watch folder

--- src/clientsocket/ClientGUI.java
+++ src/clientsocket/ClientGUI.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.*;
+import java.nio.file.*;
 import javax.swing.*;
 
 /**
@@ -20,6 +21,44 @@ public class ClientGUI {
 
     private JFrame clientFrame = new JFrame("Client");
     private JTextArea systemBoxMessage = new JTextArea(10, 20);
+
+    public void watchFolder() throws IOException {
+        WatchService watcher = FileSystems.getDefault().newWatchService();
+        Path dir = Paths.get("C:/ClientMonitoringSystem/Data");
+        dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
+        System.out.println("Watch Service registered for dir: " + dir.getFileName());
+
+        WatchKey key = null;
+        while (true) {
+            try {
+                // System.out.println("Waiting for key to be signalled...");
+                key = watcher.take();
+            } catch (InterruptedException ex) {
+                System.out.println("InterruptedException: " + ex.getMessage());
+                return;
+            }
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                // Retrieve the type of event by using the kind() method.
+                WatchEvent.Kind<?> kind = event.kind();
+                WatchEvent<Path> ev = (WatchEvent<Path>) event;
+                Path fileName = ev.context();
+                if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                    System.out.printf("A new file %s was created.%n", fileName.getFileName());
+                } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
+                    System.out.printf("A file %s was modified.%n", fileName.getFileName());
+                } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
+                    System.out.printf("A file %s was deleted.%n", fileName.getFileName());
+                }
+            }
+
+            boolean valid = key.reset();
+            if (!valid) {
+                break;
+            }
+        }
+    }
 
     public void connectServer(String ip, int port) {
         Thread connect = new Thread() {
@@ -39,6 +78,7 @@ public class ClientGUI {
                             GUIAfterConnect();
                         }
                     });
+                    watchFolder();
                 } catch (IOException ex) {
                     SwingUtilities.invokeLater(new Runnable() {
                         @Override
@@ -59,18 +99,18 @@ public class ClientGUI {
     public void GUIAfterConnect() {
         clientFrame = new JFrame("Client");
         JPanel boxMessagePanel = new JPanel();
-        
+
         boxMessagePanel.add(systemBoxMessage);
         systemBoxMessage.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
         boxMessagePanel.add(new JScrollPane(systemBoxMessage, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                 JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
         boxMessagePanel.setBorder(BorderFactory.createTitledBorder("System Message"));
-        
+
         Font font = new Font("Times New Roman", Font.BOLD, 14);
         systemBoxMessage.setFont(font);
         systemBoxMessage.setText("Connected to the server");
         systemBoxMessage.setEditable(false);
-        
+
         clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         clientFrame.add(boxMessagePanel);
         clientFrame.pack();
